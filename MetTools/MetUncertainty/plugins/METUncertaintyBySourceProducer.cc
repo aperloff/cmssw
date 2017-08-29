7eb09c9bd79185d65ce9e5ce850760e96f933159
Emily <emacdonald16@my.whitworth.edu>
2017-08-29 17:43:35 -0500

    Uses FactorizedJetCorrectionUncertainty now. Also, correctly defines corrected MET

--- MetTools/MetUncertainty/plugins/METUncertaintyBySourceProducer.cc
+++ MetTools/MetUncertainty/plugins/METUncertaintyBySourceProducer.cc
@@ -23,6 +23,7 @@ Implementation: [Notes on implementation]
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+
 // new includes
 #include "CommonTools/Utils/interface/PtComparator.h"
 #include "JetMETCorrections/Objects/interface/JetCorrectionsRecord.h"
@@ -31,8 +32,8 @@ Implementation: [Notes on implementation]
 #include "DataFormats/PatCandidates/interface/Jet.h"
 #include "DataFormats/PatCandidates/interface/MET.h"
 #include "DataFormats/Math/interface/LorentzVector.h"
-
-
+#include "CondFormats/JetMETObjects/interface/FactorizedJetCorrectionUncertainty.h"
+#include "CondFormats/JetMETObjects/interface/FactorizedJetCorrectionUncertaintyCalculator.h"
 
 //Class declaration
 class METUncertaintyBySourceProducer : public edm::EDProducer {
@@ -48,36 +49,25 @@ private:
   edm::EDGetTokenT<edm::View<pat::MET> > slimmedmetTok_ ;
   std::vector <std::string> sources_;
   std::string JECFile_;
-  int numSources_;
+  int numSources_; //do I still need?
 
   edm::Handle< edm::View<pat::Jet> > jets;
   edm::Handle< edm::View<pat::MET> > slimmedMETs;
-
-  float slimmedmet;
-  float slimmedmetJECUp;
-  float slimmedmetJECDown;
-  float genmet;
 };
 // constants, enums and typedefs
 
 // static data member definitions
 
 // constructors and destructor
-METUncertaintyBySourceProducer::METUncertaintyBySourceProducer(const edm::ParameterSet& iConfig) {//changed from no bracket and a ":"
-// JetTag_(iConfig.getParameter<edm::InputTag>("JetTag")),
-// JetTok_(consumes<edm::View<pat::Jet> >(JetTag_)),
-// slimmedMETTag_(iConfig.getParameter<edm::InputTag>("slimmedMETTag")),
-// slimmedmetTok_(consumes<edm::View<pat::MET> >(slimmedMETTag_)),
-// sources_(iConfig.getParameter<std::vector <std::string> >("sources")),
-// JECFile_(iConfig.getParameter<std::string>("JECFile")),
-// numSources_(iConfig.getParameter<int>("numSources"))
-  JetTok_         = consumes<edm::View<pat::Jet> >(iConfig.getParameter<edm::InputTag> ("JetTag"));
-  slimmedmetTok_  = consumes<edm::View<pat::MET> >(iConfig.getParameter<edm::InputTag> ("slimmedMETTag"));
-  sources_ = iConfig.getParameter<std::vector <std::string> >("sources");
-  JECFile_ = iConfig.getParameter<std::string>("JECFile");
-  numSources_ = iConfig.getParameter<int>("numSources");
-
-  produces<std::vector<pat::Jet> >(); //I have two LorentzVectors (slimmedmetJECUp(Down), and no idea how to "produce" them)
+METUncertaintyBySourceProducer::METUncertaintyBySourceProducer(const edm::ParameterSet& iConfig) :
+JetTok_(consumes<edm::View<pat::Jet> >(iConfig.getParameter<edm::InputTag>("JetTag"))),
+slimmedmetTok_(consumes<edm::View<pat::MET> >(iConfig.getParameter<edm::InputTag>("slimmedMETTag"))),
+sources_(iConfig.getParameter<std::vector <std::string> >("sources")),
+JECFile_(iConfig.getParameter<std::string>("JECFile")),
+numSources_(iConfig.getParameter<int>("numSources"))
+{
+  produces<pat::MET>( "METJECUp" ); //correct? Will this add to ntuple?
+  produces<pat::MET>( "METJECDown" ); //correct?
 }
 
 // member functions
@@ -87,78 +77,66 @@ void
 METUncertaintyBySourceProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
   using namespace edm;
-  //I have no idea how to initialize a JetCorrectorParameters and JetCorrectionUncertainty
-  //edm::ESHandle<JetCorrectionUncertaintyCollection> JetCorUncColl;//or do we want the paramters collection?? Can't imagine why...
-  //iSetup.get<JetCorrectionsRecord>().get(JetType_,JetCorUncColl);//will this work??? Who knows
-  //JetCorrectorParameters const & JetCorPar = (*JetCorUncColl)["Uncertainty"]; //also unknown...
-  //auto jecUnc = std::make_unique<JetCorrectionUncertainty>(JetCorPar);//just going to leave these in comments in case they're useful
 
   iEvent.getByToken(JetTok_, jets);
+  iEvent.getByToken(slimmedmetTok_, slimmedMETs);
+
+  auto METJECUp = std::make_unique<pat::MET>();
+  auto METJECDown = std::make_unique<pat::MET>();
   auto newJets_up  = std::make_unique<std::vector<pat::Jet>>();
   newJets_up->reserve(jets->size());
   auto newJets_down  = std::make_unique<std::vector<pat::Jet>>();
   newJets_down->reserve(jets->size());
 
-  math::XYZTLorentzVector sumJet;
-  math::XYZTLorentzVector sumJetJECUp;
-  math::XYZTLorentzVector sumJetJECDown;
+  math::XYZTLorentzVector diff_up(0,0,0,0); //reset sum(jet_corr-jet)
+  math::XYZTLorentzVector diff_down(0,0,0,0);
 
   for (edm::View<pat::Jet>::const_iterator itJet = jets->begin(); itJet != jets->end(); itJet++) {
-    sumJet+=itJet->p4();
     // construct the Jet from the ref -> save ref to original object
     unsigned int idx = std::distance(jets->begin(),itJet);
     edm::RefToBase<pat::Jet> jetRef = jets->refAt(idx);
     edm::Ptr<pat::Jet> jetPtr = jets->ptrAt(idx);
-    pat::Jet ajet_down(jetPtr);
-    pat::Jet ajet_up(jetPtr);
+    pat::Jet ajet_down(jetPtr); pat::Jet ajet_up(jetPtr);
     math::XYZTLorentzVector vjet_down = ajet_down.p4();
     math::XYZTLorentzVector vjet_up = ajet_up.p4();
 
-    //loop over chosen sources
-    for (int i=0;i<numSources_;i++){
-      //not sure if this is how you define a JetCorrectorParameter/JetCorrectionUncertainty in CMSSW
-      JetCorrectorParameters jcp(JECFile_,sources_[i]); //auto jcp  = std::make_unique<JetCorrectorParameters>(JECFile,sources[i]);
-      JetCorrectionUncertainty jcu(jcp); //auto jcu  = std::make_unique<JetCorrectionUncertainty>(jcp);
+    //Create a jcuc of all the sources per jet, set eta and pt, retrieve total uncertainty
+    //edm::ESHandle<JetCorrectionUncertaintyCollection> jcucHandle; //works? compiles. Do I need handle?
+    JetCorrectionUncertaintyCollection jcuc(JECFile_,sources_); //works? compiles
+    auto fcuc = std::make_unique<FactorizedJetCorrectionUncertainty>(jcuc);
 
-      //Get JEC unc for this jet, both up and down
-      //Need to set the pT and eta twice because resets once getUncertainty() is called
-      jcu.setJetEta(itJet->eta());
-      jcu.setJetPt(itJet->pt());   //jecUnc->setJetEta(itJet->eta()); //jecUnc->setJetPt(itJet->pt());
-      double unc_up = jcu.getUncertainty(true);
-      jcu.setJetEta(itJet->eta());
-      jcu.setJetPt(itJet->pt());
-      double unc_down = jcu.getUncertainty(false);//Does not work unless resetting the eta and pt
+    fcuc->setJetEta(itJet->eta());
+    fcuc->setJetPt(itJet->pt());
+    double tot_unc = fcuc->getUncertainty();
 
-      //safety check if uncertainty is not available for a jet
-      if(unc_up==-999.) unc_up = 0;
-      if(unc_down==-999.) unc_down = 0;
+    double jecUncScale_up = 1.0; //no variation - just store scale factor & uncertainty
+    double jecUncScale_down = 1.0; //no variation - just store scale factor & uncertainty
 
-      double jesUncScale_up = 1.0; //no variation - just store scale factor & uncertainty
-      double jesUncScale_down = 1.0; //no variation - just store scale factor & uncertainty
+    //downward variation
+    jecUncScale_down = 1 - tot_unc;
+    //upward variation
+    jecUncScale_up = 1 + tot_unc;
 
-      //downward variation
-      jesUncScale_down = 1 - unc_down;
-      //upward variation
-      jesUncScale_up = 1 + unc_up;
+    //apply variation
+    vjet_up *= jecUncScale_up;   //this is a p4
+    vjet_down *= jecUncScale_down;
 
-      //apply variation, sum up changes (not saving the jets)
-      vjet_up *= jesUncScale_up;   //this is a p4
-      vjet_down *= jesUncScale_down;
-      sumJetJECUp += vjet_up;
-      sumJetJECDown += vjet_down; //ajet_up.setP4(vjet_up); //newJets_up->push_back(ajet_up);
-    } //end sources loop
+    //sum differences between scaled jet and original
+    diff_up += (vjet_up - ajet_up.p4()); //still a p4
+    diff_down += (vjet_down - ajet_down.p4()); //still a p4
+
   } //end jets loop
 
-  //using slimmedMET. Probably want genMET (??)
-  iEvent.getByToken(slimmedmetTok_, slimmedMETs);
-  slimmedmet=slimmedMETs->at(0).pt();
-  genmet=slimmedMETs->at(0).genMET()->pt(); //unsure if correct syntax, so not using yet
-  math::XYZTLorentzVector metP4 = slimmedMETs->at(0).p4();
+  //using slimmedMET
+  //slimmedmet=slimmedMETs->at(0).pt();
+  math::XYZTLorentzVector metP4 = slimmedMETs->at(0).p4(); //slimmedMETs is handle
 
-  //how do I return these?? Should I return phi, too?
-  slimmedmetJECUp   = (metP4 + (sumJet - sumJetJECUp)).Pt();
-  slimmedmetJECDown = (metP4 + (sumJet-sumJetJECDown)).Pt();
+  //calculate "corrected" MET for both up/down variation
+  METJECUp->setP4(metP4 + diff_up); //compiles
+  METJECDown->setP4(metP4 + diff_down); //compiles
 
+  iEvent.put(std::move(METJECUp));
+  iEvent.put(std::move(METJECDown));
 } //end produce
 
 DEFINE_FWK_MODULE(METUncertaintyBySourceProducer);
